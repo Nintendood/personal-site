c1919de97d0093887d7ba387b338ce1075335333
adam <adam.jensen.harris@gmail.com>
2021-09-18 16:11:28 -0600

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Under construction...
+        </p>
+        <p>Check out my talk while you wait.</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/M_JmcrgT1DA" title="YouTube video player - Why Aren't You Making Video Games Right Now?" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
